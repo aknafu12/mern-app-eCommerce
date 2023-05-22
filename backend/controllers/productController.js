e18ed49a82314f8eb5f5170e9eb03fd74d0e0110
aknafu12 <kgebre2021@gmail.com>
2023-05-22 06:06:32 +0300

    updated for new product/POST method

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,15 @@
+const product = require('../models/product');
+const product = require('../models/product')
+
+//create new product
+exports.newProduct = async (req, res, next) =>{
+    const product = await product.create(req.body);
+    res.status(201).json({
+        message: "Product created successfully",
+        product
+    });
+}
+
 exports.getAllProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
