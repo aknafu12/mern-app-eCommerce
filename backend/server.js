43b576fe6b3216724cb24c53b99fc2168f6c235b
aknafu12 <kgebre2021@gmail.com>
2023-05-24 12:28:29 +0300

    added uncaught exception and unhandled promiseRej

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,29 @@ const port = process.env.PORT;
 //connect database
 connectDatabase();
 
-server.listen(port, () => {
+const serverHandle = server.listen(port, () => {
   console.log(`Server started on port ${port} in ${process.env.NODE_ENV}`);
 });
+
+//handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  console.log('shutting down the server due to unhandled promise rejection ');
+
+  //close server and exit process
+  serverHandler.close(() => process.exit(1));
+  });
+  //handle uncaught exceptions
+  process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    //close server and exit process
+    serverHandler.close(() => process.exit(1));
+    });
+    //handle ctrl+c event
+    process.on("SIGINT", () => {
+      console.log("Caught interrupt signal");
+      //close server and exit process
+      serverHandler.close(() => process.exit(1));
+      });
+
+
