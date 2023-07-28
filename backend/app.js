4e29e0fb5bc04235fc993be8f1410d4181f4a716
aknafu12 <kgebre2021@gmail.com>
2023-07-28 11:30:46 +0300

    added user register api

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// import all routes
 const products = require('./routes/product');
+const auth = require('./routes/authRoutes');
+
 
 
 const cors = require('cors');
@@ -13,10 +16,12 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+
 // Routes
 
 app.use('/api', products);
-app.use('/api', products);
+app.use('/api', auth);
 
 // Middleware error handling 
 app.use(errorMiddleware);
