5baac284c0ee7f196f9892ce7b166fcfff3ae6bc
aknafu12 <kgebre2021@gmail.com>
2023-07-30 19:21:12 +0300

    authenticate admin for CRUD

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,13 +5,13 @@ const {getAllProducts, addNewProduct, getSingleProduct, updateProduct, deletePro
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
 // user
-router.route('/products').get(isAuthenticatedUser, getAllProducts);
+router.route('/products').get(getAllProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 // admin
-router.route('/admin/product/new').post(addNewProduct);
-router.route('/admin/product/:id').put(updateProduct);
-router.route('/admin/product/:id').delete(deleteProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser, addNewProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser, updateProduct);
+router.route('/admin/product/:id').delete(isAuthenticatedUser, deleteProduct);
 
 
 module.exports = router;
